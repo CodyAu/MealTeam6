6fc91499f0a4d3f7f6de42432dd983bf05e1d017
Ian <EverythingElse@Ians-MacBook-Pro-3.local>
2020-11-23 11:43:28 -0600

    map marker update

--- mapFunction.js
+++ mapFunction.js
@@ -1,12 +1,30 @@
 var myRestList = document.getElementsByClassName("rlist");
+var myMarkers = document.getElementsByClassName("marker");
+
 var i;
 var n;
 for (i = 0; i < myRestList.length; i++) {
-    myRestList[i].onclick = function() {
+    myRestList[i].onclick = function restSelect() {
         for (n = 0; n < myRestList.length; n++){
             myRestList[n].classList.remove('rclicked');
+            myMarkers[n].classList.remove('mclicked');
         }
         var li = this;
         li.classList.toggle('rclicked');
+        myMarkers[li.id].classList.toggle('mclicked');
+    }
+}
+
+var m;
+var k;
+for (m = 0; m < myMarkers.length; m++) {
+    myMarkers[m].onclick = function markerSel() {
+        for (k = 0; k < myMarkers.length; k++){
+            myRestList[k].classList.remove('rclicked');
+            myMarkers[k].classList.remove('mclicked');
+        }
+        var mark = this;
+        mark.classList.toggle('mclicked');
+        myRestList[mark.id].classList.toggle('rclicked');
     }
 }
